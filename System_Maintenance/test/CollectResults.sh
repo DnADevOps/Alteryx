d1cd0fb41711bf9d49ddab81f0a341944268db72
DevOpsDemo825 <DevOpsDemo825@gmail.com>
2020-12-22 12:25:52 +0000

    Tue Dec 22 12:25:52 UTC 2020 commit

--- System_Maintenance/test/CollectResults.sh
+++ System_Maintenance/test/CollectResults.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 Maintenance='System_Maintenance'
 Environment='test'
-BuildNumber='153'
+BuildNumber='154'
 
 echo Making directory /tmp/${BuildNumber}/${Maintenance}/${Environment} recursively
 mkdir -p /tmp/${BuildNumber}/${Maintenance}/${Environment}
